9a6abaaf3cf24687315aa29b3a945e091d3b30b2
Lelamakh <makhokhashvililela@gmail.com>
2024-11-18 22:51:17 -0500

    added js file
    added elements, llength, the third elemet and push/pop funcions

---
+++ main.js
@@ -0,0 +1,16 @@
+// Array - მასივი
+
+const myElements = [10, 15, 20, 25, 30];
+console.log(myElements);
+
+const myElementsLength = myElements.length;
+console.log(myElementsLength);
+
+const myThirdElement = myElements[2];
+console.log(myThirdElement);
+
+myElements.push(35);
+console.log(myElements);
+
+const removedElement = myElements.pop();
+console.log(removedElement);
